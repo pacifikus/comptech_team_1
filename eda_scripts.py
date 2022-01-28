f64933da162293c0c26c53db4a56a73cc2900b02
Margarita Melnikova <margaritamelnikova@Margaritas-MacBook-Pro.local>
2022-01-29 00:34:27 +0300

    days before and after

--- eda_scripts.py
+++ eda_scripts.py
@@ -38,6 +38,16 @@ def get_holidays(path='./data/calendar_2021.json'):
     jsonObj = json.loads(jsonText)
     return jsonObj
 
+def get_days_before_nearest_day(row, calendar):
+    current_doy = row['doy']
+    days = min(calendar[calendar['doy'] >= current_doy]['doy']) - current_doy
+    return days
+
+def get_days_after_nearest_day(row, calendar):
+    current_doy = row['doy']
+    days = max(calendar[calendar['doy'] <= current_doy]['doy']) - current_doy
+    return days
+    
 def get_features(df):
     '''
     The function receives the dataframe and returns df with new features
@@ -49,9 +59,40 @@ def get_features(df):
     df['season'] = df['date'].dt.month.apply(lambda x: get_season(x))
     df['week'] = df['date'].dt.isocalendar().week
     df['dow'] = df['date'].dt.dayofweek
+    df['doy'] = df['date'].dt.day_of_year # нужно для других функций
     df['hour'] = df['date'].dt.hour
     df['hour_cos'] = np.cos( 2 * math.pi * df['hour'] / 24)
     df['hour_sin'] = np.sin( 2 * math.pi * df['hour'] / 24)
-    holidays = get_holidays()['holidays'] + get_holidays()['preholidays']
-    df['bank_holidays'] = np.where(df['date_date'].isin(holidays), 1, 0)    
+    bank_holidays = get_holidays()['holidays'] + get_holidays()['preholidays']
+    df['bank_holidays'] = np.where(df['date_date'].isin(bank_holidays), 1, 0)
+    
+    # before and after holidays
+    
+    ## dics
+    #### of holidays
+    official_holidays = pd.DataFrame(
+                ['2021-01-01', '2021-01-02', '2021-01-03', 
+                 '2021-01-04', '2021-01-05', '2021-01-06', 
+                 '2021-01-07', '2021-01-08', '2021-02-23', 
+                 '2021-03-08', '2021-05-01', '2021-05-09', 
+                 '2021-06-12', '2021-11-04', '2021-12-31'],
+                columns=['date'])
+    official_holidays['date'] = pd.to_datetime(official_holidays['date'])
+    official_holidays['doy'] = official_holidays['date'].dt.day_of_year
+    #### celebrations
+    celebrations = pd.DataFrame(
+                ['2021-01-01', '2021-02-23', '2021-02-14', 
+                '2021-03-08', '2021-09-01', '2021-12-31'],
+                columns=['date'])
+    celebrations['date'] = pd.to_datetime(celebrations['date'])
+    celebrations['doy'] = celebrations['date'].dt.day_of_year    
+    #### for faster performance
+    doy_df = df[['date_date', 'doy']].copy()
+    doy_df = doy_df.drop_duplicates()
+    doy_df['days_before_of_holiday'] = doy_df.apply(lambda x: get_days_before_nearest_day(x, official_holidays), axis=1)
+    doy_df['days_before_celebrations'] = doy_df.apply(lambda x: get_days_before_nearest_day(x, celebrations), axis=1)
+    doy_df['days_after_of_holiday'] = doy_df.apply(lambda x: get_days_after_nearest_day(x, official_holidays), axis=1)
+    doy_df['days_after_celebrations'] = doy_df.apply(lambda x: get_days_after_nearest_day(x, celebrations), axis=1)
+    df = df.merge(doy_df, on=['doy', 'date_date'], how='left')
+    
     return df
